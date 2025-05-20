b6c0b49089b2db7a7c62c7ae900cf02b74195bec
sagarcws11 <sagarcws11@gmail.com>
2025-05-20 18:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todo", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
